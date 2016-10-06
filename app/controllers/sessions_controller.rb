a330792d1a75c430a255b4ea3540ede704c1ddb3
calebomusic <calebomusic@gmail.com>
2016-10-06 11:21:49 -0700

    Create basic user and sessions functionality

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by_credentials(session_params[:email], session_params[:password])
+
+    if user
+      log_in_user!(user)
+      redirect_to user_url(user)
+    else
+      flash.new[:errors] = ["Invalid Credentials. Nice try."]
+      render :new
+    end
+  end
+
+  def destroy
+    user = current_user
+    log_out!(current_user)
+    render :new
+  end
+
+  private
+  def session_params
+    params.require(:user).permit(:email, :password)
+  end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+    render :show
+  end
+
+  def create
+    user = User.new(user_params)
+
+    if user.valid?
+      user.save
+      log_in_user!(user)
+      redirect_to user_url(user)
+    else
+      flash.now[:errors] = user.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+end
