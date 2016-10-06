3eca59c66a01055ac7ec3671f977f46d21298dca
calebomusic <calebomusic@gmail.com>
2016-10-06 11:33:57 -0700

    Add routes for Album, Track, Band

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   resources :users, only: [:create, :new, :show]
   resource :session, only: [:create, :new, :destroy]
+
+  resources :bands do
+    resources :albums, only: [:new]
+  end
+
+  resources :albums, only: [:create, :destroy, :update, :show, :edit] do
+    resources :tracks, only: [:new]
+  end
+
+  resources :tracks, only: [:create, :destroy, :update, :show, :edit] 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
